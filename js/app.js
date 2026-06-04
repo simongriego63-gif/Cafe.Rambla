68e10693774eecaa41024ef772fb24757ec76968
simongriego63-gif <simongriego63@gmail.com>
2026-06-04 00:09:21 -0300

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import { db } from './firebase-config.js';
+import { db } from ' ../firebase-config.js';
 import { doc, setDoc, getDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
 
 class RamblaApp {

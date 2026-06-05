91f94605652b9100611d83aba873af563a79c635
simongriego63-gif <simongriego63@gmail.com>
2026-06-04 21:44:08 -0300

    Add Service Worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,70 @@
+const CACHE_NAME = 'rambla-cache-v1'; // Cambiamos el nombre para forzar a los celulares a descartar la versión "beki"
+
+// Lista de archivos clave que el celular debe guardar en memoria
+const urlsToCache = [
+  './',
+  './index.html',
+  './caja.html',
+  './admin.html',
+  './manifest.json',
+  './caja.json',   
+  './admin.json',   
+  './icon.png',
+  
+  // Archivos de las carpetas
+  './css/style.css',
+  './css/caja.css',
+  './js/firebase-config.js',
+  './js/app.js',
+  './js/caja.js'
+];
+
+// 1. INSTALACIÓN: Guarda los archivos en caché
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Archivos de club rambla cacheados correctamente');
+        return cache.addAll(urlsToCache);
+      })
+  );
+  // Fuerza al Service Worker a activarse inmediatamente
+  self.skipWaiting(); 
+});
+
+// 2. ACTIVACIÓN: Limpia cachés viejos si actualizas la app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Borrando caché antiguo:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// 3. FETCH: Sirve los archivos desde el caché para que cargue rapidísimo
+self.addEventListener('fetch', (event) => {
+  // Ignoramos las peticiones a Firebase y a la API del código QR para que siempre carguen en vivo
+  if (
+    event.request.url.includes('firestore.googleapis.com') || 
+    event.request.url.includes('identitytoolkit') ||
+    event.request.url.includes('api.qrserver.com')
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Devuelve el archivo del caché si existe, si no, lo pide a internet
+        return response || fetch(event.request);
+      })
+  );
+});
